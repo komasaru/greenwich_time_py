ea251f48c77d7d77c4ec3891cc1a249726ca7d48
Masaru Koizumi <masaru@mk-mode.com>
2016-06-24 13:59:59 +0900

    UPD: Moved 'nut_ls.txt' and 'nut_pl.txt' into 'lib' directory.

--- lib/greenwich.rb
+++ lib/greenwich.rb
@@ -14,19 +14,20 @@ module Greenwich
   include FundamentalArgument
   include Matrix
 
-  DAT_LS = "nut_ls.txt"
-  DAT_PL = "nut_pl.txt"
-  J2000  = 2451545.0                      # Reference epoch (J2000.0), Julian Date
-  JC     = 36525.0                        # Days per Julian century
-  TT_TAI = 32.184                         # TT - TAI
-  DAYSEC = 86400.0                        # Seconds per a day
-  AS2R   = 4.848136811095359935899141e-6  # Arcseconds to radians
-  PI     = 3.141592653589793238462643     # PI
-  PI2    = 6.283185307179586476925287     # 2 * PI
-  PI_180 = 0.017453292519943295           # PI / 180
-  TURNAS = 1296000.0                      # Arcseconds in a full circle
-  U2R    = AS2R / 1.0e7                   # Units of 0.1 microarcsecond to radians
-  R2D    = 57.29577951308232087679815     # Radians to degrees
-  D2S    = 3600.0                         # Degrees to seconds
+  DIR_LIB = File.expand_path('../greenwich', __FILE__)
+  DAT_LS  = "#{DIR_LIB}/nut_ls.txt"
+  DAT_PL  = "#{DIR_LIB}/nut_pl.txt"
+  J2000   = 2451545.0                      # Reference epoch (J2000.0), Julian Date
+  JC      = 36525.0                        # Days per Julian century
+  TT_TAI  = 32.184                         # TT - TAI
+  DAYSEC  = 86400.0                        # Seconds per a day
+  AS2R    = 4.848136811095359935899141e-6  # Arcseconds to radians
+  PI      = 3.141592653589793238462643     # PI
+  PI2     = 6.283185307179586476925287     # 2 * PI
+  PI_180  = 0.017453292519943295           # PI / 180
+  TURNAS  = 1296000.0                      # Arcseconds in a full circle
+  U2R     = AS2R / 1.0e7                   # Units of 0.1 microarcsecond to radians
+  R2D     = 57.29577951308232087679815     # Radians to degrees
+  D2S     = 3600.0                         # Degrees to seconds
 end
 
